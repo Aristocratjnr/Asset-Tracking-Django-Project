51e42656dd67dc433c52b3ded1d231419900ef4e
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-11 15:14:04 +0000

    feat: Add profile page and update registration process

--- users/urls.py
+++ users/urls.py
@@ -22,7 +22,8 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.Register, name='register'),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('asset_app.urls')),
