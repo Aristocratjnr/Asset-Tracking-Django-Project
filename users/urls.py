963baab4412d764b857044b2b15794b8d762294e
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-25 15:50:27 +0000

    Update Dashboard URL in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
      path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('SignIn',auth_view.LoginView.as_view(template_name='SignIn.html')),
     path('Dashboard/index', views.Dashboard),
+    path('Dashboard', views.DashboardAPI.as_view()),
 ]
