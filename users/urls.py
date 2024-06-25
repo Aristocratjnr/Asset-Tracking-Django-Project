2362f95a6226713240ff7bb0d83343fcf5f79575
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-25 15:49:02 +0000

    refactor: Update logout URL in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('logout/', auth_view.LogoutView.as_view(template_name='logout.html')),
+     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('SignIn',auth_view.LoginView.as_view(template_name='SignIn.html')),
     path('Dashboard/index', views.Dashboard),
 ]
