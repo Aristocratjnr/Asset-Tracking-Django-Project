b76e9c52bcf5e804b6ca19fb1a88ea702e617448
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-12 22:48:13 +0000

    refactor: Update models.py in users app for improved structure and consistency

--- users/models.py
+++ users/models.py
@@ -1,27 +1,46 @@
-# views.py
-from django.shortcuts import render
-from sklearn import model_selection
-from .models import Block, House, Payment, Sale
 
-def dashboard(request):
-    total_blocks = Block.objects.count()
-    total_houses = House.objects.count()
-    total_occupied_houses = House.objects.filter(occuppied=True).count()
-    total_payments = Payment.objects.count()
-    total_sales = Sale.objects.count()
-    total_sales_amount = Sale.objects.aggregate(model_selection.Sum('price'))['price__sum']
+from django.db import models
+from django.contrib.auth.models import User
 
-    recent_payments = Payment.objects.order_by('-payment_id')[:5]
-    recent_sales = Sale.objects.order_by('-id')[:5]
+class MyAppUser( models.Model ) :
+    def __unicode__( self ) :
+       return self.user.username
+    user    = models.ForeignKey( User, on_delete=models.CASCADE )
+    house_no = models.CharField(max_length=255, null=True, unique=True)
+    block_no = models.CharField(max_length=255, null=True)
+    national_id  = models.IntegerField(null=True)
 
-    context = {
-        'total_blocks': total_blocks,
-        'total_houses': total_houses,
-        'total_occupied_houses': total_occupied_houses,
-        'total_payments': total_payments,
-        'total_sales': total_sales,
-        'total_sales_amount': total_sales_amount,
-        'recent_payments': recent_payments,
-        'recent_sales': recent_sales,
-    }
-    return render(request, 'dashboard.html', context)
+
+
+# Create your models here.
+
+
+class Block(models.Model):
+    block_no = models.CharField(max_length=255, primary_key=True)
+    capacity = models.IntegerField()
+
+class House(models.Model):
+    house_no = models.CharField(max_length=255, primary_key=True)
+    block_no = models.ForeignKey( Block, on_delete=models.CASCADE )
+    occuppied = models.BooleanField(default=False)
+    rent = models.IntegerField()
+    size = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+
+
+class Payment(models.Model):
+    payment_id = models.IntegerField( primary_key=True, auto_created=True)
+    amount = models.IntegerField()
+    house_no = models.ForeignKey(House, on_delete=models.CASCADE)
+    date = models.IntegerField()
+    month = models.CharField(max_length=255)
+    year = models.CharField(max_length=4)
+    username = models.CharField(max_length=255)
+    national_id = models.IntegerField()
+
+class Sale(models.Model):
+    item = models.CharField(max_length=100)
+    price = models.FloatField()
+ 
+    def __str__(self):
+        return str(self.item)
