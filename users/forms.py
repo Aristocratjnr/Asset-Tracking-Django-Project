75b178c7841567c44b43e757418d07568e20d7ac
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-13 18:14:09 +0000

    refactor: Update add_asset.html to improve consistency and remove unnecessary code

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,9 @@
+from ast import Assert
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+
+from dashboard.views import add_asset
 
 
 class UserRegisterForm(UserCreationForm):
@@ -9,3 +12,8 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1']
+
+class AssetForm(forms.ModelForm):
+    class Meta:
+        model = add_asset
+        fields = ['name', 'category', 'assigned_to', 'cost']
--- users/models.py
+++ users/models.py
@@ -44,3 +44,12 @@ class Sale(models.Model):
  
     def __str__(self):
         return str(self.item)
+    
+class Asset(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    assigned_to = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
