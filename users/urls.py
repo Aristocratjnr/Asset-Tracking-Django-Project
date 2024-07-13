f6bf6aed77d6dc059b16e5c55a9f82b486c11724
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-13 18:35:43 +0000

    refactor: Update URLs and templates for adding assets

--- users/urls.py
+++ users/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
      path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('SignIn',auth_view.LoginView.as_view(template_name='SignIn.html')),
      path('Assets', views.Dashboard),
+     path('add_asset', views.Dashboard),    
     path('Dashboard/index', views.Dashboard),
     path('Dashboard', views.DashboardAPI.as_view()),
 ]
